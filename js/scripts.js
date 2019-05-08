0a6f66013f444c6ddaf9eaa51c34c507a4c61b75
Damien <damosullivan@gmail.com>
2019-05-08 08:44:10 +0000

    reverted url, added screenshot

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // const url = window.location.host || "localhost";
-    const url = "Netlify Placeholder"
+    const url = window.location.host || "localhost";
     document.getElementById("website-name").textContent = url;
     document.title = url + " | damosullivan/netlify-placeholder";
 }, false);
