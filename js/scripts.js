dfe32a0ada71f06e828d020ac0ff018e12651810
Damien <damosullivan@gmail.com>
2019-05-08 08:41:32 +0000

    to revert

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const url = window.location.host || "localhost";
-    url = "Netlify Placeholder"
+    // const url = window.location.host || "localhost";
+    const url = "Netlify Placeholder"
     document.getElementById("website-name").textContent = url;
     document.title = url + " | damosullivan/netlify-placeholder";
 }, false);
